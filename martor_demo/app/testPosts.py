9b27200550e769506303312f135791186bd1c653
Jaloliddin <d.murodullaev@easybooking.uz>
2023-09-26 12:11:48 +0500

    tests have been changed

--- martor_demo/app/testPosts.py
+++ martor_demo/app/testPosts.py
@@ -3,7 +3,7 @@ from app.models import Post
 
 class TestPostCase(TestCase):
     def testPost(self):
-        post = Post(title="My Title", description="Blurb", wiki="Post Body")
+        post = Post(title="My Title", description="Kali", wiki="Post Body")
         self.assertEqual(post.title, "My Title")
-        self.assertEqual(post.description, "Blurb")
+        self.assertEqual(post.description, "Kali")
         self.assertEqual(post.wiki, "Post Body")
